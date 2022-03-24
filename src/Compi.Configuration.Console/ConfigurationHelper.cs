77c7174dcf83851a71b4f2e96ab41b5a3cac5258
Eduardo Muñoz Villagrán <emunoz@microexpertos.com>
2022-03-23 22:21:39 -0300

    Add project files.

---
+++ src/Compi.Configuration.Console/ConfigurationHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compi.Configuration.ConsoleApp
+{
+    public static class ConfigurationHelper
+    {
+
+        public static string GetParameterFromIniFile(string variableName, string filePath)
+        {
+            string result = "";          
+
+            if (!File.Exists(filePath))
+                throw new FileNotFoundException(filePath);
+            
+            using var streamFile = File.OpenText(filePath);
+
+            while (!streamFile.EndOfStream)
+            {
+                string lineReaded = streamFile.ReadLine();
+                if (!string.IsNullOrEmpty(lineReaded))
+                {
+                    string[] lineSplited = lineReaded.Split("=");
+                    if (lineSplited[0].Equals(variableName, StringComparison.OrdinalIgnoreCase))                                                   
+                        return lineSplited[1];                        
+                }
+            }
+    
+
+            return result;
+
+        }
+    }
+}
---
+++ src/Compi.Configuration.Console/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Compi.Configuration.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            const string configFileName = "configuration.ini";
+
+            var sender = ConfigurationHelper.GetParameterFromIniFile("sender", configFileName);
+            var password = ConfigurationHelper.GetParameterFromIniFile("password", configFileName);
+            var smtp = ConfigurationHelper.GetParameterFromIniFile("smtp", configFileName);
+
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("#################################");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine($"Sender: {sender}");
+            Console.WriteLine($"Password: {password}");
+            Console.WriteLine($"SMTP: {smtp}");
+
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("#################################");
+            Console.ForegroundColor = ConsoleColor.White;
+
+
+
+        }
+    }
+}
