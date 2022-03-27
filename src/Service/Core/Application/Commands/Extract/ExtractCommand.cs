7a89729fb8836dd1790f69455d209fda00a46df7
Eduardo Muñoz Villagrán <emunoz@microexpertos.com>
2022-03-27 20:33:53 -0300

    other command

---
+++ src/Service/Core/Application/Commands/Extract/ExtractCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compi.Configuration.Service.Core.Application.Commands.Extract
+{
+    internal class ExtractCommand
+    {
+    }
+}
