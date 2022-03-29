12c0919822336295ca5593df1b1144286a6b5768
Eduardo Muñoz Villagrán <emunoz@microexpertos.com>
2022-03-29 00:40:51 -0300

    little refactor

--- src/ConsoleApp/Program.cs
+++ src/ConsoleApp/Program.cs
@@ -11,58 +11,7 @@ namespace Compi.Configuration.ConsoleApp
         {
             ConfigurationClassicWay();
 
-          
-
-            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-
-            IConfigurationBuilder configurationBuilder =
-                new ConfigurationBuilder()               
-                .AddJsonFile($"appsettings.json",
-                             optional: false,
-                             reloadOnChange: true)
-                .AddJsonFile($"appsettings.{environment}.json",
-                             optional: true,
-                             reloadOnChange: true)
-                .AddCommandLine(args);
-               
-
-
-            IConfigurationRoot configuration = configurationBuilder.Build();
-
-
-           // Task.Delay(5000).Wait();
-
-            var var1 = configuration["Var1"];
-
-            var var2 = configuration.GetSection("vAr1").Value;
-
-            var var3 = configuration.GetRequiredSection("vaR1").Value.ToString();
-
-          
-
-           // var settings = configuration.GetSection(nameof(Settings)).Get<Settings>();
-
-
-
-            Settings settings = new ()
-            {
-                Var1 = "default value"
-            };
-
-
-            configuration.GetSection(nameof(Settings)).Bind(settings);
-
-
-
-
-
-
-            Console.WriteLine($"var1: {var1}");
-
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("#################################");
-            Console.ForegroundColor = ConsoleColor.White;
-            
+            ConfigurationWithConfigurationApi(args);
 
 
             // Console.ReadLine();
@@ -79,7 +28,56 @@ namespace Compi.Configuration.ConsoleApp
 
         }
 
+        private static void ConfigurationWithConfigurationApi(string[] args)
+        {
 
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+            IConfigurationBuilder configurationBuilder =
+                new ConfigurationBuilder()
+                .AddJsonFile($"appsettings.json",
+                             optional: false,
+                             reloadOnChange: true)
+                .AddJsonFile($"appsettings.{environment}.json",
+                             optional: true,
+                             reloadOnChange: true)
+                .AddCommandLine(args);
+
+
+
+            IConfigurationRoot configuration = configurationBuilder.Build();
+
+
+            // Task.Delay(5000).Wait();
+
+            var var1 = configuration["Var1"];
+
+            var var2 = configuration.GetSection("vAr1").Value;
+
+            var var3 = configuration.GetRequiredSection("vaR1").Value.ToString();
+
+
+
+            // var settings = configuration.GetSection(nameof(Settings)).Get<Settings>();
+
+
+
+            Settings settings = new()
+            {
+                Var1 = "default value"
+            };
+
+
+            configuration.GetSection(nameof(Settings)).Bind(settings);
+
+
+
+            Console.WriteLine($"var1: {var1}");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("#################################");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
 
 
         private static void ConfigurationClassicWay()
